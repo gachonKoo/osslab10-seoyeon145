42fa56d04b48e80beabf7c1aebf30a47eb97f4a0
KIMseoyeon <rlatjdus5198@naver.com>
2024-11-11 00:03:50 +0900

    Update divisiors.py

--- divisiors.py
+++ divisiors.py
@@ -1,10 +1,11 @@
 import sys
 
 number = int(sys.argv[1])
-divisors = []
 
 for i in range(1, number + 1):
     if number % i == 0:
-        divisors.append(i)
+        print(i, end=" ")
 
-print(*divisors)
+print()
+
+(oss) $ python divisors.py 100
